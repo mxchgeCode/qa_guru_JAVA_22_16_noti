28b6e9cb45dcfc195777daa3f500b6d924503dd4
mtx <dadbarn@gmail.com>
2023-10-22 01:03:12 +0300

    update logs

--- src/test/java/tests/TestBaseRemote.java
+++ src/test/java/tests/TestBaseRemote.java
@@ -18,7 +18,6 @@ public class TestBaseRemote {
     @BeforeAll
     static void init() {
         String browserFromProperties = System.getProperty("browser", "chrome_100.0");
-//        String browserFromProperties = System.getProperty("browser", "firefox_98.0");
         String[] browserInfo = browserFromProperties.split("_");
         String browserFromPropertiesName = browserInfo[0];
         String browserFromPropertiesVersion = browserInfo[1];
@@ -50,7 +49,6 @@ public class TestBaseRemote {
     void addArtefactsToReport() {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
-//        Attach.browserConsoleLogs();
         Attach.addVideo();
         closeWebDriver();
     }
